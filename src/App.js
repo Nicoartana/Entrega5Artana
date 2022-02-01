46549683c6592a76df6f78ba8a5c4ce7f57c8f27
Nicoartana <95253750+Nicoartana@users.noreply.github.com>
2022-01-31 21:39:21 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import NavBar from './Components/Navbar';
+import ItemCount from "./Components/ItemCount"
+import ItemListContainer from './Components/ItemListContainer';
+
+
+
+const App = () => {
+  return (
+      <><NavBar /><ItemCount /></>
+      
+      
+      
+      
+  );
+  
+  
+};
+
+
+
+
+export default App;
+
+
+
